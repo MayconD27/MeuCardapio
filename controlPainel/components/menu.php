e42ba2c75b88744b423e85a29477674a2ea32203
MayconD27 <mdsx627@gmail.com>
2024-12-09 21:22:38 -0300

    criado side bar

---
+++ controlPainel/components/menu.php
@@ -0,0 +1,27 @@
+<div class="d-flex flex-column flex-shrink-0 bg-light" style="width: 6.5rem; height:100vh;">
+            <a href="/VaraQuebrada/controlPainel" class="d-block p-3 link-dark text-decoration-none logo"  data-bs-toggle="tooltip" data-bs-placement="right">
+                <img src="../img/logo.png" alt="logo">
+            </a>
+            <ul class="nav nav-pills nav-flush flex-column mb-auto text-center">
+                <li class="nav-item">
+                <a href="#" class="nav-link active py-3 border-bottom" aria-current="page" title="Home" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <i class="bi bi-house"></i> 
+                </a>
+                </li>
+                <li>
+                <a href="#" class="nav-link py-3 border-bottom" title="Dashboard" data-bs-toggle="tooltip" data-bs-placement="right">
+                    <i class="bi bi-patch-plus"></i>
+                </a>
+                </li>
+            </ul>
+            <div class="dropdown border-top">
+                <a href="#" class="d-flex align-items-center justify-content-center p-3 link-dark text-decoration-none dropdown-toggle" id="dropdownUser3" data-bs-toggle="dropdown" aria-expanded="false">
+                <img src="https://github.com/mdo.png" alt="mdo" width="34" height="34" class="rounded-circle">
+                </a>
+                <ul class="dropdown-menu text-small shadow" aria-labelledby="dropdownUser3">
+                <li><a class="dropdown-item" href="#">Configurações</a></li>
+                <li><hr class="dropdown-divider"></li>
+                <li><a class="dropdown-item" href="#">Sair</a></li>
+                </ul>
+            </div>
+</div>
